65435b6e19ccf59dc269f4fa9dd22b104c474392
yukpiz <yukpiz@gmail.com>
2017-01-31 10:32:54 +0900

    Updated script.

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
 
 def lambda_handler(event, context):
-    print('hogehoge')
+    url = 'http://www.google.co.jp'
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        print 'OK'
+    else:
+        print 'NG'
+
+if __name__ == "__main__":
+    lambda_handler({}, {})
